cd465bf55032bb2e7d23461cdf601c1043e7da41
bob-fed <bovafedoruk@gmail.com>
2025-06-29 14:26:55 +0300

    Initialize Project and Apply Improvements

---
+++ server/middleware/validation.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
+
+export const handleValidationErrors = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      message: 'Validation Error',
+      errors: errors.array().map(error => ({
+        field: error.param,
+        message: error.msg,
+        value: error.value
+      }))
+    });
+  }
+  next();
+};
